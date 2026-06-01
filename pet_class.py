18f6e6447752a5249e1b18bc4026a9c67b413d11
yanab <yanaborja610@gmail.com>
2026-06-01 20:54:25 +0800

    Added encapsulation for the attributes and also created the setters

--- pet_class.py
+++ pet_class.py
@@ -1,5 +1,15 @@
 class Pet:
     def __init__(self, name = "", animal_type = "", age = 0):
-        self.name = name
-        self.animal_type = animal_type
-        self.age = age
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+
+    # Setters
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
