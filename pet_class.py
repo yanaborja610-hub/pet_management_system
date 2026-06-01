e07f1cec74dfe74ee0fecaa5ecd5440ed62fb63d
yanab <yanaborja610@gmail.com>
2026-06-01 23:10:54 +0800

    Fixed the indentations on the method save file

--- pet_class.py
+++ pet_class.py
@@ -48,23 +48,23 @@ class Pet:
                 if pet.get_animal_type() not in animal_types:
                     animal_types.append(pet.get_animal_type())
 
-                for animal in animal_types:
-                    file.write(f"\n{animal.upper()}S\n")
-                    file.write("-" * 60 + "\n")
-                    file.write(
-                        f"{'PET NAME': <25}"
-                        f"{'ANIMAL TYPE': <20}"
-                        f"{'AGE': <10}\n"
-                    )
+            for animal in animal_types:
+                file.write(f"\n{animal.upper()}S\n")
+                file.write("-" * 60 + "\n")
+                file.write(
+                    f"{'PET NAME': <25}"
+                    f"{'ANIMAL TYPE': <20}"
+                    f"{'AGE': <10}\n"
+                )
 
-                    file.write("-" * 60 + "\n")
+                file.write("-" * 60 + "\n")
 
-                    for pet in pets:
-                        if pet.get_animal_type() == animal:
-                            file.write(
-                                f"{pet.get_name():<25}"
-                                f"{pet.get_animal_type():<20}"
-                                f"{pet.get_age():<10}\n"
-                            )
+                for pet in pets:
+                    if pet.get_animal_type() == animal:
+                        file.write(
+                            f"{pet.get_name():<25}"
+                            f"{pet.get_animal_type():<20}"
+                            f"{pet.get_age():<10}\n"
+                        )
 
-                    file.write("\n")
+                file.write("\n")
